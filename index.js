6cca70ce68439d4d9771aea6de134aabf7c389aa
aguiraf <aguiraf@gmail.com>
2020-09-24 10:40:50 +0200

    Add cron job

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict"
-const geojsonVt = require('geojson-vt');
-const vtPbf = require('vt-pbf');
-const request = require('requestretry');
-const zlib = require('zlib');
+const geojsonVt = require('geojson-vt')
+const vtPbf = require('vt-pbf')
+const request = require('requestretry')
+const zlib = require('zlib')
+const CronJob = require('cron').CronJob
 
 const query = `
   query bikerentals {
@@ -16,7 +17,22 @@ const query = `
   }`
 
 class GeoJSONSource {
-  constructor(uri, callback){
+
+
+  constructor(uri, callback) {
+
+    this.updateTileIndex(uri)
+
+    const job = new CronJob('*/15 * * * * *', () => {
+      this.updateTileIndex(uri)
+    })
+    job.start()
+
+    callback(null, this)
+  }
+
+
+  updateTileIndex(uri) {
     uri.protocol = "http:"
     request({
       url: uri,
@@ -27,52 +43,53 @@ class GeoJSONSource {
       headers: {
         'Content-Type': 'application/graphql'
       }
-    }, function (err, res, body){
-      if (err){
+    }, (err, res, body) => {
+      if (err) {
         console.log(err)
-        callback(err);
-        return;
+        return
       }
 
-      const geoJSON = {type: "FeatureCollection", features: JSON.parse(body).data.bikeRentalStations.map(station => ({
-        type: "Feature",
-        geometry: {type: "Point", coordinates: [station.lon, station.lat]},
-        properties: {
-          id: station.stationId,
-          name: station.name,
-          networks: station.networks.join()
-        }
-      }))}
+      const geoJSON = {
+        type: "FeatureCollection", features: JSON.parse(body).data.bikeRentalStations.map(station => ({
+          type: "Feature",
+          geometry: { type: "Point", coordinates: [station.lon, station.lat] },
+          properties: {
+            id: station.stationId,
+            name: station.name,
+            networks: station.networks.join()
+          }
+        }))
+      }
 
       this.tileIndex = geojsonVt(geoJSON, {
         maxZoom: 20,
         buffer: 256
-      }); //TODO: this should be configurable
+      }) //TODO: this should be configurable
       console.log("city bikes loaded from:", uri.host + uri.path)
-      callback(null, this)
-    }.bind(this));
-  };
-
-  getTile(z, x, y, callback){
-    let tile = this.tileIndex.getTile(z, x, y)
-
-    if (tile === null){
-      tile = {features: []}
-    }
-
-    const data = Buffer.from(vtPbf.fromGeojsonVt({stations: tile}));
-
-    zlib.gzip(data, function (err, buffer) {
-      if (err){
-        callback(err);
-        return;
-      }
-
-      callback(null, buffer, {"content-encoding": "gzip"})
     })
   }
 
-  getInfo(callback){
+
+  getTile(z, x, y, callback) {
+    let tile = this.tileIndex.getTile(z, x, y)
+
+    if (tile === null) {
+      tile = { features: [] }
+    }
+
+    const data = Buffer.from(vtPbf.fromGeojsonVt({ stations: tile }));
+
+    zlib.gzip(data, function (err, buffer) {
+      if (err) {
+        callback(err)
+        return;
+      }
+
+      callback(null, buffer, { "content-encoding": "gzip" })
+    })
+  }
+
+  getInfo(callback) {
     callback(null, {
       format: "pbf",
       vector_layers: [{
@@ -91,3 +108,6 @@ module.exports = GeoJSONSource
 module.exports.registerProtocols = (tilelive) => {
   tilelive.protocols['otpcitybikes:'] = GeoJSONSource
 }
+
+
+// git add .; git commit -m 'modifica n. '; git push
